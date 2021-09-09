bb3156b33c53cff160afb79a04415d458da40579
blendthink <info@blendthink.dev>
2021-09-10 02:37:43 +0900

    set main of root to flex 1

--- src/jsMain/kotlin/io/github/blendthink/style/AppStylesheet.kt
+++ src/jsMain/kotlin/io/github/blendthink/style/AppStylesheet.kt
@@ -2,12 +2,24 @@ package io.github.blendthink.style
 
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.css.selectors.CSSSelector
+import org.jetbrains.compose.web.css.selectors.id
+import org.jetbrains.compose.web.css.selectors.type
 
 object AppStylesheet : StyleSheet() {
     init {
 
         CSSSelector.Universal style {
             margin(0.px)
+        }
+
+        id("root") style {
+            display(DisplayStyle.Flex)
+            flexFlow(FlexDirection.Column, FlexWrap.Wrap)
+            minHeight(100.vh)
+
+            type("main") style {
+                flex(1)
+            }
         }
 
         media(mediaMaxWidth(640.px)) {
