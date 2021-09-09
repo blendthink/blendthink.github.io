54ac88e28a5668e1ce0b0544db099d26091b70e1
blendthink <info@blendthink.dev>
2021-09-10 02:46:54 +0900

    set Header, Main, Footer

--- src/jsMain/kotlin/io/github/blendthink/App.kt
+++ src/jsMain/kotlin/io/github/blendthink/App.kt
@@ -1,40 +1,19 @@
 package io.github.blendthink
 
 import org.jetbrains.compose.web.css.Style
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import io.github.blendthink.content.PageFooter
+import io.github.blendthink.content.PageHeader
+import io.github.blendthink.content.PageMain
 import io.github.blendthink.style.AppStylesheet
-import org.jetbrains.compose.web.css.padding
-import org.jetbrains.compose.web.css.px
-import org.jetbrains.compose.web.dom.Button
-import org.jetbrains.compose.web.dom.Div
-import org.jetbrains.compose.web.dom.Span
-import org.jetbrains.compose.web.dom.Text
 import org.jetbrains.compose.web.renderComposable
 
 fun main() {
-    var count: Int by mutableStateOf(0)
 
     renderComposable(rootElementId = "root") {
         Style(AppStylesheet)
 
-        Div({ style { padding(25.px) } }) {
-            Button(attrs = {
-                onClick { count -= 1 }
-            }) {
-                Text("-")
-            }
-
-            Span({ style { padding(15.px) } }) {
-                Text("$count")
-            }
-
-            Button({
-                onClick { count += 1 }
-            }) {
-                Text("+")
-            }
-        }
+        PageHeader()
+        PageMain()
+        PageFooter()
     }
 }
