84646f0965981e80455f693d949ba5d5d42176e0
blendthink <info@blendthink.dev>
2021-09-10 02:43:16 +0900

    create PageMain

---
+++ src/jsMain/kotlin/io/github/blendthink/content/PageMain.kt
@@ -0,0 +1,11 @@
+package io.github.blendthink.content
+
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.web.dom.Main
+
+@Composable
+fun PageMain() {
+    Main {
+        
+    }
+}
