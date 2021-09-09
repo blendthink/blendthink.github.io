449c3a568413ab6772f81d8a20805671c8cf4bbc
blendthink <info@blendthink.dev>
2021-09-10 00:07:26 +0900

    update kotlin, compose, gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 
 plugins {
-    kotlin("multiplatform") version "1.5.10"
-    id("org.jetbrains.compose") version "0.5.0-build226"
+    kotlin("multiplatform") version "1.5.30"
+    id("org.jetbrains.compose") version "1.0.0-alpha4-build331"
 }
 
 repositories {
