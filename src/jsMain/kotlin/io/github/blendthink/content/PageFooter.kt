07d05c30dcb5a6b59df7b5af9d737543f884fc7b
blendthink <info@blendthink.dev>
2021-09-10 02:20:06 +0900

    create PageFooter

---
+++ src/jsMain/kotlin/io/github/blendthink/content/PageFooter.kt
@@ -0,0 +1,28 @@
+package io.github.blendthink.content
+
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.dom.Footer
+import org.jetbrains.compose.web.dom.Span
+import org.jetbrains.compose.web.dom.Text
+
+@Composable
+fun PageFooter() {
+    Footer({
+        style {
+            padding(20.px)
+            backgroundColor(rgb(50, 50, 54))
+        }
+    }) {
+        Span({
+            style {
+                display(DisplayStyle.Block)
+                textAlign("center")
+                color(rgba(255, 255, 255, 0.3))
+                fontSize(0.7.cssRem)
+            }
+        }) {
+            Text("Copyright © 2021 blendthink All rights reserved.")
+        }
+    }
+}
