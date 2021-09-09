d22a4422df1fa43fa57a7aac1b41ed1987191675
blendthink <info@blendthink.dev>
2021-09-10 02:44:00 +0900

    create PageHeader

---
+++ src/jsMain/kotlin/io/github/blendthink/content/PageHeader.kt
@@ -0,0 +1,11 @@
+package io.github.blendthink.content
+
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.web.dom.Header
+
+@Composable
+fun PageHeader() {
+    Header {
+
+    }
+}
