ee60a41558dd6f3e5244447bd3392e32b4e3cabc
blendthink <info@blendthink.dev>
2021-09-10 09:18:31 +0900

    add okayaman icon to main block

--- src/jsMain/kotlin/io/github/blendthink/content/PageMain.kt
+++ src/jsMain/kotlin/io/github/blendthink/content/PageMain.kt
@@ -1,11 +1,26 @@
 package io.github.blendthink.content
 
 import androidx.compose.runtime.Composable
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.Img
 import org.jetbrains.compose.web.dom.Main
 
 @Composable
 fun PageMain() {
     Main {
-        
+        Div({
+            style {
+                textAlign("center")
+            }
+        }) {
+            Img(src = "okayaman.png", alt = "おかやまん") {
+                style {
+                    width(6.em)
+                    borderRadius(50.percent)
+                }
+            }
+
+        }
     }
 }
